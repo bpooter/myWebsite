b896accb049cf05993f080f4bbaaa61a01cf636c
bpooter <brandonpotter9225@yahoo.com>
2026-07-05 01:42:42 -0400

    Added initial bio for Brandon

--- group_bio_template/script.js
+++ group_bio_template/script.js
@@ -1,14 +1,22 @@
 /**
  * Toggles the display of an individual team member's bio
  * @param {string} bioId - The ID of the bio section to show or hide
+ * @param {string} imgId1 - The ID of the img section to show or hide
+ * @param {string} imgId2 - The ID of the img section to show or hide
  */
-function toggleBio(bioId) {
+function toggleBio(bioId, imgId1, imgId2) {
     const bio = document.getElementById(bioId);
+    const img1 = document.getElementById(imgId1)
+    const img2 = document.getElementById(imgId2)
     // Toggle between showing and hiding the bio section
     if (bio.style.display === "none" || bio.style.display === "") {
         bio.style.display = "block";
+        img1.style.display = "block";
+        img2.style.display = "block";
     } else {
         bio.style.display = "none";
+        img1.style.display = "none";
+        img2.style.display = "none";
     }
 }
 
